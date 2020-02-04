35b7c5d54d2173337360d1d8ff60a571c397cebd
ejarzo <ejarz25@gmail.com>
2020-02-04 00:18:49 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Rita from 'rita';
+import Tone from 'tone';
+
 import './App.css';
+import { useFreesound } from './useFreesound';
+
+const player = new Tone.Player({ autostart: true });
+player.send('masterOutput');
+
+const masterCompressor = new Tone.Compressor({
+  ratio: 16,
+  threshold: -30,
+  release: 0.25,
+  attack: 0.003,
+  knee: 30,
+});
+
+const masterLimiter = new Tone.Limiter(-3);
+const masterReverb = new Tone.Reverb(0.8);
+const masterDelay = new Tone.Freeverb();
+const masterOutput = new Tone.Gain(0.8).receive('masterOutput');
+
+masterOutput.chain(masterCompressor, masterLimiter, Tone.Master);
 
 function App() {
+  const [query, setQuery] = useState(Rita.randomWord());
+  const { soundUrl, imageUrl, tags } = useFreesound(query);
+
+  useEffect(() => {
+    if (!soundUrl) return;
+    player.load(soundUrl);
+  }, [soundUrl]);
+
+  const phonemes = Rita.getPhonemes(query);
+  const rhymes = Rita.rhymes(query);
+
+  const handleClick = () => {
+    const rhyme = rhymes[Math.floor(Math.random() * rhymes.length)];
+    setQuery(rhyme);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          color: 'white',
+          position: 'absolute',
+          background: '#111',
+          top: 5,
+          left: 5,
+          padding: 10,
+          zIndex: 3,
+        }}
+      >
+        <div>
+          <button
+            onClick={() => {
+              setQuery(Rita.randomWord());
+            }}
+          >
+            Random
+          </button>
+        </div>
+        query: {query}
+        <br />
+        rhymes: {rhymes.join(',')}
+        <br />
+        tags: {tags.join(',')}
+      </div>
+      <div
+        onClick={handleClick}
+        style={{
+          color: 'white',
+          position: 'absolute',
+          display: 'flex',
+          width: '100%',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: '5em',
+          zIndex: 2,
+        }}
+      >
+        {phonemes.replace(/-/g, '')}
+      </div>
+      <img
+        style={{ display: 'block', height: '100%', width: '100%' }}
+        src={imageUrl}
+        alt=""
+      />
     </div>
   );
 }
---
+++ src/useFreesound.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import useSWR from 'swr';
+
+const { REACT_APP_FREESOUND_API_KEY } = process.env;
+const BASE_URL = 'https://freesound.org/apiv2';
+
+const fetcher = url => fetch(url).then(r => r.json());
+
+export const useFreesound = query => {
+  const [{ soundUrl, imageUrl, tags }, setFreesoundData] = useState({
+    soundUrl: '',
+    imageUrl: '',
+    tags: [],
+  });
+  const [activeFreesoundId, setActiveFreesoundId] = useState(null);
+
+  // search for sound
+  const { data: searchData, error: searchError } = useSWR(
+    `${BASE_URL}/search/text/?token=${REACT_APP_FREESOUND_API_KEY}&format=json&query=${query}`,
+    fetcher
+  );
+
+  console.log('search data', searchData);
+  // get sound data
+  const { data: soundData, error: soundError } = useSWR(
+    activeFreesoundId
+      ? `${BASE_URL}/sounds/${activeFreesoundId}/?token=${REACT_APP_FREESOUND_API_KEY}`
+      : null,
+    fetcher
+  );
+
+  useEffect(() => {
+    if (searchData && searchData.results && searchData.results.length > 0) {
+      const { results } = searchData;
+      const { id } = results[Math.floor(Math.random() * results.length)];
+      setActiveFreesoundId(id);
+    }
+  }, [searchData]);
+
+  useEffect(() => {
+    if (!soundData) return;
+    const {
+      images: { spectral_l: imageUrl },
+      previews: { 'preview-lq-mp3': soundUrl },
+      tags,
+    } = soundData;
+    setFreesoundData({
+      soundUrl,
+      imageUrl,
+      tags,
+    });
+  }, [soundData, query]);
+
+  return {
+    soundUrl,
+    imageUrl,
+    searchError,
+    soundError,
+    tags,
+  };
+};
